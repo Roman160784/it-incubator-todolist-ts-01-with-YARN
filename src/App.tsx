cfb1bed6d8879568507c36baed87324bb3bf97b6
Roman160784 <matveyenkaraman@gmail.com>
2021-11-14 15:57:53 +0300

    14.11.2021

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,39 @@ import ToDoList from './ToDoList'
 
 
 export type TaskType = {
-title: string
-isDone: boolean
-id: number
+    title: string
+    isDone: boolean
+    id: number
 }
 
 function App() {
 
-const todoListTitle_1: string = 'What I learn'
-const todoListTitle_2: string = 'What I have'
-const todoListTitle_3: string = 'What I have to buy'
+    const todoListTitle_1: string = 'What I learn'
+    const todoListTitle_2: string = 'What I have'
+    const todoListTitle_3: string = 'What I have to buy'
 
-const task_1: Array<TaskType> =[
-    {id: 1, title:'HTML', isDone: true},
-    {id: 2, title:'CSS', isDone: true},
-    {id: 3, title:'REACT', isDone: false},
-]
-const task_2: Array<TaskType> =[
-    {id: 4, title:'Fish', isDone: true},
-    {id: 5, title:'Beef', isDone: true},
-    {id: 6, title:'Butter', isDone: true},
-]
-const task_3: Array<TaskType> =[
-    {id: 7, title:'Water', isDone: true},
-    {id: 8, title:'Juice', isDone: false},
-    {id: 9, title:'Cheese', isDone: true},
-]
+    const task_1: Array<TaskType> = [
+        { id: 1, title: 'HTML', isDone: true },
+        { id: 2, title: 'CSS', isDone: true },
+        { id: 3, title: 'REACT', isDone: false },
+    ]
+    const task_2: Array<TaskType> = [
+        { id: 4, title: 'Fish', isDone: true },
+        { id: 5, title: 'Beef', isDone: true },
+        { id: 6, title: 'Butter', isDone: true },
+    ]
+    const task_3: Array<TaskType> = [
+        { id: 7, title: 'Water', isDone: true },
+        { id: 8, title: 'Juice', isDone: false },
+        { id: 9, title: 'Cheese', isDone: true },
+    ]
 
     return (
-       <div className='App'>
-<ToDoList title={todoListTitle_1} tasks={task_1}/>
-<ToDoList title={todoListTitle_2} tasks={task_2}/>
-<ToDoList title={todoListTitle_2} tasks={task_3}/>
-       </div>
+        <div className='App'>
+            <ToDoList title={todoListTitle_1} tasks={task_1} />
+            <ToDoList title={todoListTitle_2} tasks={task_2} />
+            <ToDoList title={todoListTitle_3} tasks={task_3} />
+        </div>
     );
 }
 
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import App from './App';
-import {TaskType} from './App'
+import { TaskType } from './App'
 
 type PropsTupe = {
-title: string
-tasks: Array<TaskType>
+    title: string
+    tasks: Array<TaskType>
 }
 
-function ToDoList (props: PropsTupe) {
+function ToDoList(props: PropsTupe) {
     return (
 
-<div>
-<h3>{props.title}</h3>
-<div>
- <input/>
-<button>+</button>
-</div>
-<ul>
-<li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-<li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-<li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
-</ul>
-<div>
-<button>All</button>
-<button>Active</button>
-<button>Completed</button>
-</div>
-</div>
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input />
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={props.tasks[0].isDone} /> <span>{props.tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[1].isDone} /> <span>{props.tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={props.tasks[2].isDone} /> <span>{props.tasks[2].title}</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
 
     );
 }
