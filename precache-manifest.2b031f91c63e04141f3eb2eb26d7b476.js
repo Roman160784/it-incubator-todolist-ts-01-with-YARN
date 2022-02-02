d86f9cf67d10d6daa63cef8c738b852f802a9ba5
Roman160784 <matveyenkaraman@gmail.com>
2022-02-02 16:33:21 +0300

    Updates

---
+++ precache-manifest.2b031f91c63e04141f3eb2eb26d7b476.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ac3db8657a736f3b81e578fecdc7c27",
+    "url": "/it-incubator-todolist-ts-01-with-YARN/index.html"
+  },
+  {
+    "revision": "36f5d1953ee94f6fdedb",
+    "url": "/it-incubator-todolist-ts-01-with-YARN/static/css/main.297e91a7.chunk.css"
+  },
+  {
+    "revision": "31d1e88a818999c521a5",
+    "url": "/it-incubator-todolist-ts-01-with-YARN/static/js/2.074baa51.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/it-incubator-todolist-ts-01-with-YARN/static/js/2.074baa51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36f5d1953ee94f6fdedb",
+    "url": "/it-incubator-todolist-ts-01-with-YARN/static/js/main.ec79135d.chunk.js"
+  },
+  {
+    "revision": "e59629e6de8211cec1de",
+    "url": "/it-incubator-todolist-ts-01-with-YARN/static/js/runtime-main.e43ce57b.js"
+  }
+]);
